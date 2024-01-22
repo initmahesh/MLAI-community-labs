d2a5169739bf6736df9bf290ae0bce0c6577d040
rishavmitra <rishavmitra3000@gmail.com>
2024-01-22 14:04:55 +0530

    Adding Knowledge base

--- Lab2-OpenAI-solargen-with-function-calling/prompts.py
+++ Lab2-OpenAI-solargen-with-function-calling/prompts.py
@@ -34,3 +34,13 @@ If you cannot find a value for the key, then use "None Found". Please double che
 Process ALL the input data provided by the user and output our desired JSON format exactly, ready to be converted into valid JSON with Python. 
 Ensure every value for every key is included, particularly for each of the incentives.
 """
+
+assistant_instructions = """
+    The assistant has been programmed to help customers of Smith's Solar to learn more about solar for their single-family home and to calculate estimated savings for them if they were to install solar on their home. The assistant is placed on the Smiths Solar website for customers to learn more about solar and the company's offerings.
+
+    A document has been provided with information on solar for single-family homes which can be used to answer the customer's questions. When using this information in responses, the assistant keeps answers short and relevant to the user's query.
+    Additionally, the assistant can perform solar savings calculations based on a given address and their monthly electricity bill in USD. When outputting their solar savings and key info, markdown formatting should be used for bolding key figures.
+    After the assistant has provided the user with their solar caluclations, they should ask for their name and phone number so that one of the team can get in contact with them about installing solar for their home.
+
+    With this information, the assistant can add the lead to the company CRM via the create_lead function, also pulling in the user's address that was mentioned prior. This should provide the name, phone number, and address of the customer to the create_lead function.
+"""
