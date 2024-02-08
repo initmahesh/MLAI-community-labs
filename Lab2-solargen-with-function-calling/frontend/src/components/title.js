8e2456d9f0b354c51d5423837118c880600060cd
rishavmitra <rishavmitra3000@gmail.com>
2024-02-08 12:47:59 +0530

    New files

---
+++ Lab2-solargen-with-function-calling/frontend/src/components/title.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+function App() {
+    return (
+        <div>
+            <Helmet>
+                <title>New Title Here</title>
+            </Helmet>
+            {/* Your app content */}
+        </div>
+    );
+}
+
+export default App;
